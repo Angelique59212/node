a8e452a641436f5c810862960e94e3d5a4c20300
angelika <dehainaut.angelique@orange.fr>
2023-05-02 10:46:51 +0200

    middleware

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,20 @@ const expressApp = express();
 const server = http.createServer(expressApp);
 
 //Premier Middleware qui a comme charge de logger dans la console une request envoyé par le client
-expressApp.use((req, res) => {
-    res.json({message: "Hello World !"});
+expressApp.use((req, res, next) => {
+    console.log("Requête reçue => " + req.url);
+    next();
+});
+
+// second middleware qui est en charge d'envoyer une réponse au client.
+expressApp.use((req, res, next) => {
     res.send('Hello World !');
-    res.redirect('https://www.google.fr');
+    next();
+});
+
+// troisième middleware pour exécuter un log une fois la requếte terminée
+expressApp.use((req, res) => {
+    console.log("Requête terminée, réponse envoyé au client");
 });
 
 expressApp.set('port', process.env.PORT || 3000);
